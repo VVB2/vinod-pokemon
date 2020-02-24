2670c9ae64211fd0e72108fa069474aec46b5996
VVB2 <51293918+VVB2@users.noreply.github.com>
2020-02-24 11:27:45 +0530

    improvements

--- pokemon.js
+++ pokemon.js
@@ -1,20 +1,30 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-  // The type of chart we want to create
-  type: 'radar',
-
-  // The data for our dataset
-  data: {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [{
-      label: 'My First dataset',
-      backgroundColor: 'rgb(0, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45]
-    }]
-  },
-
-  // Configuration options go here
-  options: {}
-    }}
-});
+// var ctx = document.getElementById('myChart').getContext('2d');
+// var url = "https://github.com/VVB2/pokemon.json/blob/master/pokedex.json";
+// fetch("../items.json").then(function (response) {
+//     return response.json();
+//   }).then(function (obj){
+//     console.log(obj);
+//     document.getElementsByClassName("main").
+//   }).catch(function (error) {
+//   console.log("error");
+//   console.log(error);
+// });
+// var chart = new Chart(ctx, {
+//   // The type of chart we want to create
+//   type: 'radar',
+//
+//   // The data for our dataset
+//   data: {
+//     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//     datasets: [{
+//       label: 'My First dataset',
+//       backgroundColor: 'rgb(0, 99, 132)',
+//       borderColor: 'rgb(255, 99, 132)',
+//       data: [0, 10, 5, 2, 20, 30, 45]
+//     }]
+//   },
+//
+//   // Configuration options go here
+//   options: {}
+// });
+// console.log("hello");
